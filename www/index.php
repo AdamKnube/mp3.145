d71cb85724a211852b1b1b20e477d0bcdc116371
AdamKnube <adamk@5csecurity.com>
2014-06-21 16:36:57 -0400

    modified:   www/index.php (added search)

--- www/index.php
+++ www/index.php
@@ -7,14 +7,15 @@
 		<center><h1><font color="#0000FF">Mp3.145 Web Interface</font></h1></center>
 		<br>
 		<?php echo '<center>';
-			if ($_GET['p']) { system('/home/mp3/play'); }
-			elseif ($_GET['u']) { system('/home/mp3/volume_up_10'); }
-			elseif ($_GET['d']) { system('/home/mp3/volume_down_10'); }
-			elseif ($_GET['b']) { system('/home/mp3/back'); }
-			elseif ($_GET['n']) { system('/home/mp3/next'); }
-			elseif ($_GET['r']) { system('/home/mp3/random'); }
-			elseif ($_GET['f']) { system('/home/mp3/force ' . $_GET['song']); }
-			echo '</center><br>';
+			if ($_GET['p']) { system('/home/mp3/bin/play'); }
+			elseif ($_GET['u']) { system('/home/mp3/bin/volume_up_10'); }
+			elseif ($_GET['d']) { system('/home/mp3/bin/volume_down_10'); }
+			elseif ($_GET['b']) { system('/home/mp3/bin/back'); }
+			elseif ($_GET['n']) { system('/home/mp3/bin/next'); }
+			elseif ($_GET['r']) { system('/home/mp3/bin/random'); }
+			elseif ($_GET['f']) { system('/home/mp3/bin/force ' . $_GET['song']); }
+			elseif ($_GET['s']) { system('/home/mp3/bin/search ' . $_GET['query']); }
+			echo '</center><br><br>';
 		?>
 		<form method="GET" action="/index.php">
 				<center>
@@ -29,12 +30,15 @@
 					<input type="text" value="" name="song">
 					<input type="submit" value="Force" name="f">
 				</center><br><center>
+					<input type="text" value="" name="query">
+					<input type="submit" value="Search" name="s">
+				</center><br><center>
 					<input type="submit" value="Random On/Off" name="r">
 				</center>				
 			<br><br>
 		</form><center>
 			<h2><u><font color="#00FF00">Song List:</font></u></h2>
-			<?php system('/home/mp3/list'); ?>
+			<?php system('/home/mp3/bin/list'); ?>
 		</center>
 	</body>
 </html>
