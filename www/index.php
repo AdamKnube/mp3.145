6a128a5ed84a0c830c972881533f7f67ac9b253c
AdamKnube <adamk@5csecurity.com>
2014-06-23 19:26:18 -0400

    modified:   03-sockserv (more info from status command 'c' and added more html to the 'l' command) 	modified:   clients/search (bugfix... who knew 'Found' could be in a song name?) 	modified:   www/index.php (made it all mobile phone friendly, looks way big on a real screen)

--- www/index.php
+++ www/index.php
@@ -2,7 +2,7 @@
 	<head>
 		<title>Mp3.145 Web Interface</title>
 	</head>
-	<body bgcolor="#000000" text="#CACACA">
+	<body bgcolor="#000000" text="#CACACA" style="font-size: 50px">
 		<br><br>
 		<center><h1><font color="#0000FF">Mp3.145 Web Interface</font></h1></center>
 		<br>
@@ -19,23 +19,23 @@
 			echo '</center><br><br>';
 		?>
 		<form method="GET" action="/index.php">
-				<center>
-					<input type="submit" value="Play/Pause" name="p">
+				<font size=+10><center>
+					<input type="submit" value="Play/Pause" name="p" style="width: 400px; font-size: 50px; height: 120px"/>
+				</center><br><center>
+					<input type="submit" value="Random On/Off" name="r" style="width: 400px; font-size: 50px; height: 120px"/>
 				</center><br><center>	
-					<input type="submit" value="Back" name="b">
-					<input type="submit" value="Next" name="n">
+					<input type="submit" value="Back" name="b" style="width: 400px; font-size: 50px; height: 120px"/>
+					<input type="submit" value="Next" name="n" style="width: 400px; font-size: 50px; height: 120px"/>
 				</center><br><center>
-					<input type="submit" value="Volume -10" name="d">
-					<input type="submit" value="Volume +10" name="u">
+					<input type="submit" value="Volume -10" name="d" style="width: 400px; font-size: 50px; height: 120px"/>
+					<input type="submit" value="Volume +10" name="u" style="width: 400px; font-size: 50px; height: 120px"/>
 				</center><br><center>
-					<input type="text" value="" name="song">
-					<input type="submit" value="Force" name="f">
+					<input type="text" value="" name="song" style="width: 400px; height: 120px"/>
+					<input type="submit" value="Force" name="f" style="width: 400px; font-size: 50px; height: 120px"/>
 				</center><br><center>
-					<input type="text" value="" name="query">
-					<input type="submit" value="Search" name="s">
-				</center><br><center>
-					<input type="submit" value="Random On/Off" name="r">
-				</center>				
+					<input type="text" value="" name="query" style="width: 400px; height: 120px"/>
+					<input type="submit" value="Search" name="s" style="width: 400px; font-size: 50px; height: 120px"/>
+				</center></font>				
 			<br><br>
 		</form><center>
 			<h2><u><font color="#00FF00">Song List:</font></u></h2>
