8662f37b06f31d562ea7bfb0fad1f4944c58decf
Adam Knube <adamk@5csecurity.com>
2015-08-12 15:41:05 -0400

    modified:   www/index.php

--- www/index.php
+++ www/index.php
@@ -1,7 +1,7 @@
 <html>
 	<head>
 		<title>Mp3.145 Web Interface</title>
-		<meta http-equiv="refresh" content="60; url=http://192.168.210.100/index.php">
+		<meta http-equiv="refresh" content="300; url=http://192.168.200.104/index.php">
 		<style>
 			td { 
 				padding: 10px;
@@ -12,27 +12,29 @@
 	<body bgcolor="#000000" text="#CACACA" style="font-size: 50px">
 		<center><h1><font color="#0000FF">Mp3.145 Web Interface</font></h1></center>
 		<?php 
+			$query = escapeshellarg($_POST['query']);
+			$volume = escapeshellarg($_POST['volume']);
+			$what = escapeshellarg($_POST['what']);
 			if ($_POST['edge']) { exec('/home/thepub/bin/edge > /dev/null &'); }
 			elseif ($_POST['fm96']) { exec('/home/thepub/bin/fm96 > /dev/null &'); }
 			elseif ($_POST['srch']) { 
 				echo '<center>';
-				system('/home/thepub/bin/search ' . $_POST['query']);
+				system('/home/thepub/bin/search ' . $query);
 				echo '</center><br>';
 			}
-			elseif ($_POST['play']) { exec('/home/thepub/bin/play ' . $_POST['song'] . ' > /dev/null &'); }
-			elseif ($_POST['volume']) { exec('/home/thepub/bin/volume ' . $_POST['volume']);  }
+			elseif ($_POST['volume']) { exec('/home/thepub/bin/volume ' . $volume);  }
 			elseif ($_POST['stop']) { exec('/home/thepub/bin/stop'); }
-			elseif ($_GET['force']) { exec('/home/thepub/bin/play ' . $_GET['what'] . ' > /dev/null &'); }
+			elseif ($_GET['force']) { exec('/home/thepub/bin/play ' . $what . ' > /dev/null &'); }
 		?>
-		<form method="POST" action="http://192.168.210.100/">
+		<form method="POST" action="http://192.168.200.104/">
 			<center><table border=1 width=80%>
 				<tr><?php system('/home/thepub/bin/radio-info'); ?></tr>
 				<tr>
-					<td colspan=2 width=50%><input type="submit" value="Edge" name="edge" style="width: 400px; font-size: 50px; height: 120px"/></td>
-					<td colspan=2 width=50%><input type="submit" value="FM96" name="fm96" style="width: 400px; font-size: 50px; height: 120px"/></td>
-				</tr><tr>
 					<td colspan=2 width=50% align=center><input type="text" value="" name="query" style="width: 400px; height: 120px"/></td>
 					<td colspan=2 width=50% align=center><input type="submit" value="Search" name="srch" style="width: 400px; font-size: 50px; height: 120px"/></td>
+				</tr><tr>
+					<td colspan=2 width=50%><input type="submit" value="Edge" name="edge" style="width: 400px; font-size: 50px; height: 120px"/></td>
+					<td colspan=2 width=50%><input type="submit" value="FM96" name="fm96" style="width: 400px; font-size: 50px; height: 120px"/></td>
 				</tr><tr>
 					<td width=25% align=center><input type="submit" value="25%" name="volume" style="width: 200px; font-size: 50px; height: 120px"/></td>
 					<td width=25% align=center><input type="submit" value="50%" name="volume" style="width: 200px; font-size: 50px; height: 120px"/></td>
