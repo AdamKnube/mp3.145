4f5e88ffc62b89343926437f6f2d5cc0ec0e7ff2
AdamKnube <adamk@5csecurity.com>
2015-08-19 17:03:01 -0400

    modified:   bin/edge (/home/http) 	modified:   bin/fm96 (/home/http) 	modified:   bin/play (/home/http) 	modified:   bin/search (/home/http) 	modified:   www/index.php (variable server address and page name, also security fix)

--- www/index.php
+++ www/index.php
@@ -1,34 +1,40 @@
 <html>
 	<head>
 		<title>Mp3.145 Web Interface</title>
-		<meta http-equiv="refresh" content="300; url=http://192.168.200.104/index.php">
-		<style>
-			td { 
-				padding: 10px;
-				text-align: center; 
-			}
-		</style>
-	</head>
-	<body bgcolor="#000000" text="#CACACA" style="font-size: 50px">
-		<center><h1><font color="#0000FF">Mp3.145 Web Interface</font></h1></center>
-		<?php 
-			$query = escapeshellarg($_POST['query']);
-			$volume = escapeshellarg($_POST['volume']);
-			$what = escapeshellarg($_POST['what']);
-			if ($_POST['edge']) { exec('/home/thepub/bin/edge > /dev/null &'); }
-			elseif ($_POST['fm96']) { exec('/home/thepub/bin/fm96 > /dev/null &'); }
+		<?php
+			$thispage = $_SERVER['SERVER_ADDR'] . '/' . $_SERVER['PHP_SELF'];
+			echo "<meta http-equiv=\"refresh\" content=\"300\"; url=\"http://$thispage\">";
+			echo '<style>
+				td { 
+					padding: 10px;
+					text-align: center; 
+				}
+			</style>
+		</head>';
+	echo '<body bgcolor="#000000" text="#CACACA" style="font-size: 50px">
+		<center><h1><font color="#0000FF">Mp3.145 Web Interface</font></h1></center>';
+			if ($_POST['edge']) { exec('/home/http/bin/edge > /dev/null &'); }
+			elseif ($_POST['fm96']) { exec('/home/http/bin/fm96 > /dev/null &'); }
 			elseif ($_POST['srch']) { 
 				echo '<center>';
-				system('/home/thepub/bin/search ' . $query);
+	 			$query = escapeshellarg($_POST['query']);
+				system('/home/http/bin/search ' . $query);
 				echo '</center><br>';
 			}
-			elseif ($_POST['volume']) { exec('/home/thepub/bin/volume ' . $volume);  }
-			elseif ($_POST['stop']) { exec('/home/thepub/bin/stop'); }
-			elseif ($_GET['force']) { exec('/home/thepub/bin/play ' . $what . ' > /dev/null &'); }
+			elseif ($_POST['volume']) { 
+				$volume = escapeshellarg($_POST['volume']);
+				exec('/home/http/bin/volume ' . $volume);
+			}
+			elseif ($_POST['stop']) { exec('/home/http/bin/stop'); }
+			elseif ($_GET['force']) { 
+				$what = escapeshellarg($_GET['what']);
+				exec('/home/http/bin/play ' . $what . ' > /dev/null &'); 
+			}
+		
+			echo "<form method=\"POST\" action=\"http://$thispage\">";
 		?>
-		<form method="POST" action="http://192.168.200.104/">
 			<center><table border=1 width=80%>
-				<tr><?php system('/home/thepub/bin/radio-info'); ?></tr>
+				<tr><?php system('/home/http/bin/radio-info'); ?></tr>
 				<tr>
 					<td colspan=2 width=50% align=center><input type="text" value="" name="query" style="width: 400px; height: 120px"/></td>
 					<td colspan=2 width=50% align=center><input type="submit" value="Search" name="srch" style="width: 400px; font-size: 50px; height: 120px"/></td>
